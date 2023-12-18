139cd5bda186713aefd071d4b83ecbbac4ef18b5
alejo-martinez <alejoomartinex11@gmail.com>
2023-12-18 13:29:23 -0300

    probando respuesta del current

--- src/middlewares/strategy.middleware.js
+++ src/middlewares/strategy.middleware.js
@@ -1,10 +1,15 @@
 import passport from "passport";
+import CustomError from "../errors/custom.error.js";
 
 export const strategyPassport = (strategy)=>{
     return async(req, res, next) =>{
         passport.authenticate(strategy,function(err, user, info){
             if(err) next(err);
-            if(!user) next(info.message)
+            if(!user){
+                req.user = undefined;
+                // const error = new CustomError('No user', 'No user logueddd', 6);
+                return next();
+            }
             else{
                 req.user = user
                 return next()
