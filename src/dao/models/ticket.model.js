014cfdd81e6ede61cd781f2224d9a7c059db2d0e
alejo-martinez <alejoomartinex11@gmail.com>
2024-01-02 19:58:39 -0300

    ..

--- src/dao/models/ticket.model.js
+++ src/dao/models/ticket.model.js
@@ -1,16 +1,16 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
-const collection = 'tickets';
+// const collection = 'tickets';
 
-const enumPayment = ['efectivo', 'debito', 'credito', 'transferencia']
+// const enumPayment = ['efectivo', 'debito', 'credito', 'transferencia']
 
-const schema = new mongoose.Schema({
-    products: {type: mongoose.Schema.Types.ObjectId, ref: 'products'},
-    quantity: {type: Number, required:true},
-    amount: {type: Number, required: true},
-    owner: {type: mongoose.Schema.Types.ObjectId, ref: 'users'},
-    payment_method: {enum: enumPayment, required: true},
-    date: {type: Date}
-})
+// const schema = new mongoose.Schema({
+//     products: {type: mongoose.Schema.Types.ObjectId, ref: 'products'},
+//     quantity: {type: Number, required:true},
+//     amount: {type: Number, required: true},
+//     owner: {type: mongoose.Schema.Types.ObjectId, ref: 'users'},
+//     payment_method: {enum: enumPayment, required: true},
+//     date: {type: Date}
+// })
 
-export const ticketModel = mongoose.model(collection, schema);
+// export const ticketModel = mongoose.model(collection, schema);
--- src/dao/service/ticket.service.js
+++ src/dao/service/ticket.service.js
@@ -1,20 +1,20 @@
-import { ticketModel } from "../models/ticket.model.js";
+// import { ticketModel } from "../models/ticket.model.js";
 
-export class TicketManager {
+// export class TicketManager {
 
-    static async getTickets(){
-        return await ticketModel.find().lean();
-    }
+//     static async getTickets(){
+//         return await ticketModel.find().lean();
+//     }
 
-    static async getTicketById(id){
-        return await ticketModel.findOne({_id: id}).lean();
-    }
+//     static async getTicketById(id){
+//         return await ticketModel.findOne({_id: id}).lean();
+//     }
 
-    static async create(ticket){
-        await ticketModel.create(ticket);
-    }
+//     static async create(ticket){
+//         await ticketModel.create(ticket);
+//     }
 
-    static async deleteTicket(id){
-        await ticketModel.deleteOne({_id: id});
-    }
-}
+//     static async deleteTicket(id){
+//         await ticketModel.deleteOne({_id: id});
+//     }
+// }
