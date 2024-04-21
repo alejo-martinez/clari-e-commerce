658ec3ab54dc04920df05054f119b9d25523baaa
alejo-martinez <alejoomartinex11@gmail.com>
2024-04-21 13:52:12 -0300

    fix cors

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ app.use(cookieParser(config.secretCookieCode));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.use(cors({origin:['https://clari-ecommerce-front.vercel.app', 'https://api.mercadopago.com'], credentials:true, methods:'GET, POST, PUT, DELETE'}));
+app.use(cors({origin:['https://clari-ecommerce-front.vercel.app/', 'https://api.mercadopago.com'], credentials:true, methods:'GET, POST, PUT, DELETE'}));
 
 
 app.use('/static', express.static(utils.__dirname + '/public'));
